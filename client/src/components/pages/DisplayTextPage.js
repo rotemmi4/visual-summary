439b54dc8bf215108e6e8650a1728f10ca196de2
rotemmi4 <miarar@post.bgu.ac.il>
2021-01-12 20:57:33 +0200

    update radio2

--- client/src/components/pages/DisplayTextPage.js
+++ client/src/components/pages/DisplayTextPage.js
@@ -10,11 +10,41 @@ export default function DisplayTextPage(){
     const { id } = useParams()
     const text = textRepository.useGetTextWeights(id)
     const [type, setType] = useState(0)
-    
+
+    const [radio, setRadio] = useState("only Text");
+
+    let textType=0
+
+    // handleClick() {
+    //     this.setRadio(state => ({
+    //       isToggleOn: !state.isToggleOn
+    //     }));
+    //   }
+
     return(
         <>
+        <form>
+            {/* <h1>Drop Down Value is : {dropdown}</h1> */}
+            {/* <select value={dropdown} onChange={(e)=>{setDropdown(e.target.value)}}>
+                <option value="apple">Apple</option>
+                <option value="orange">Orange</option>
+                <option value="banana">Banana</option>       
+            </select> */}
+
             {/* radio buttons every change in radio button change type*/}
-            {text && text.data ? <TextVisualitaion sentences={text.data.sentences} type={1} /*type={type}*/ name={text.data.name}/> : null}
+
+            <br/>
+            <h3>Radio burron is : {radio}</h3>
+            <input type="radio" checked={radio === "WithOutVisualitation"} value="WithOutVisualitation" onChange={(e)=>{setRadio(e.target.value)}}/>
+            <label>WithOutVisualitation  </label>
+            <br/>
+            <input type="radio" checked={radio === "Gadual Highlight"} value="Gadual Highlight" onChange={(e)=>{setRadio(e.target.value)}} onClick            />
+            <label>Gadual Highlight  </label>
+
+            {text && text.data ? <TextVisualitaion sentences={text.data.sentences} type={value} /*type={type}*/ name={text.data.name}/> : null}
+
+       </form>
+
         </>
     )
 
