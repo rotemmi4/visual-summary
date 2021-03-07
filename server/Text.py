6a0ec37265a5970f352878abc2670ee7ed513470
rotemmi4 <44234304+rotemmi4@users.noreply.github.com>
2021-03-07 20:47:55 +0200

    Delete server directory

--- server/Text.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class Text(BaseModel):
-    id: int
-    name: str
-    content: str
--- server/repositories/VisualizationPropertiesRepository.py
+++
@@ -1,4 +0,0 @@
-from model.concrete import VisualizationPropertiesConcrete
-
-def visualization_properties(properties):
-    return VisualizationPropertiesConcrete.insert_visualization_properties(properties)
