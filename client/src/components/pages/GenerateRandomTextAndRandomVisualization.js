8d16c12aea31111a5a44001fe097a2135565c6e4
YassminAvraham <yasminav@post.bgu.ac.il>
2021-03-21 15:52:35 +0200

    Random Text

--- client/src/components/pages/GenerateRandomTextAndRandomVisualization.js
+++ client/src/components/pages/GenerateRandomTextAndRandomVisualization.js
@@ -23,43 +23,26 @@ export default function GenerateRandomTextAndRandomVisualization() {
             <Container>
                 <h2 className="mb-3 text-left">Test</h2><br/>
                 <text>Generate Random Texts And Random Visualizations</text><br/>
-                <Row className="justify-content-center">
-                    <Col></Col>
-                    <Col xs="9">
-                        <div>
-                            {
-                                dropdown.map((value, index) => {
-                                    return (
-                                        <>
-                                            <select value={dropdown[index]} onChange={(e)=>{
-                                                let arr=[...dropdown]
-                                                arr[index] = parseInt(e.target.value)
-                                                setDropdown(arr)
-                                            }}>
-                                                {texts && texts.data ? texts.data.map(text => (
-                                                    <option value={text.id}>{text.name}</option>
-                                                )) : null}
-                                            </select>
-                                            {'    '}<Button onClick={(e)=>{
-                                            let arr=[...modalShow]
-                                            arr[index] = true
-                                            setModalShow(arr)
-                                        }}>Show Visualization</Button><br/><br/>
-                                            <VisualizationDisplayModal show={modalShow[index]} onHide={() => {
-                                                let arr=[...modalShow]
-                                                arr[index] = false
-                                                setModalShow(arr)
-                                            }} text={dropdown[index]}></VisualizationDisplayModal>
-                                        </>
-                                    )
-                                })
-                            }
-                        </div>
+                {texts && texts.data ? texts.data.map(text => (
+                    <Row className="justify-content-center">
+                        <Col>
+                            <p>{text.name}</p>
+                        </Col>
+                        <Col>
+                            <Button onClick={(e)=>{
+                                let arr=[...modalShow]
+                                arr[text.id] = true
+                                setModalShow(arr)
+                            }}>Choose Visualization</Button><br/><br/>
+                            <VisualizationDisplayModal show={modalShow[text.id]} onHide={() => {
+                                let arr=[...modalShow]
+                                arr[text.id] = false
+                                setModalShow(arr)
+                            }} text={text.id}></VisualizationDisplayModal>
+                        </Col>
+                    </Row>
 
-
-                    </Col>
-                    <Col></Col>
-                </Row>
+                )) : null}
                 <Button className="btn btn-primary">SAVE TEST</Button><br/><br/><br/>
             </Container>
         </>);
