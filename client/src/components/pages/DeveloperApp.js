5ccc8ec930abdab263d091725cc33cb787b30fb4
YassminAvraham <yasminav@post.bgu.ac.il>
2021-04-12 22:59:58 +0300

    marge with master

--- client/src/components/pages/DeveloperApp.js
+++ client/src/components/pages/DeveloperApp.js
@@ -10,6 +10,10 @@ import CreateNewTestOptions from "./CreateNewTestOptions";
 import ChooseTestAndVisualization from "./ChooseTestAndVisualization";
 import GenerateRandomTextsAndChooseVisualization from "./GenerateRandomTextAndChooseVisualization";
 import GenerateRandomTextAndRandomVisualization from "./GenerateRandomTextAndRandomVisualization";
+import TextManagement from "./TextManagement";
+import DeleteText from "./DeleteTextPage"
+import AddQuestion from "./AddQuestion";
+import DeleteQuestion from "./DeleteQuestion";
 
 function AuthenticatedApp() {
 
@@ -20,6 +24,11 @@ function AuthenticatedApp() {
                 <Route path='/' exact component={HomePage} />
                 <Route path='/testManagement' component={TestManagement} />
                 <Route path='/new_test_options' component={CreateNewTestOptions} />
+                <Route path='/add-text' component={TextManagement} />
+                <Route path='/delete-text' component={DeleteText} />
+                {<Route path='/add-question' component={AddQuestion} />}
+                {<Route path='/delete-question' component={DeleteQuestion} />}
+                {/*<Route path='/texts/:id' component={} />*/}
                 <Route path='/ChooseTextsAndVisualizations' component={ChooseTestAndVisualization} />
                 <Route path='/GenerateRandomTextsAndChooseVisualizations' component={GenerateRandomTextsAndChooseVisualization} />
                 <Route path='/GenerateRandomTextsAndRandomVisualizations' component={GenerateRandomTextAndRandomVisualization} />
