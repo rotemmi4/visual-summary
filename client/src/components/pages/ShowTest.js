cf6fa5e5f0328c04f5ea2c71b4781ec808927189
YassminAvraham <yasminav@post.bgu.ac.il>
2021-04-23 18:40:23 +0300

    path to show test

--- client/src/components/pages/ShowTest.js
+++ client/src/components/pages/ShowTest.js
@@ -1,25 +1,50 @@
 import React, { useEffect, useState } from 'react';
+import {useParams, useLocation} from "react-router-dom";
 
 import {Button, Col, Container, Row} from "react-bootstrap";
+import * as testRepository from "../../repositories/TestRepository";
+import {RandomVisualizationDisplayModal} from "../RandomVisualizationDispalyModal";
 
 
 
 export default function ShowTest() {
-    // let location = useLocation();
-    // let testName = location.state.testName
+     let location = useLocation();
+     let testName = location.state.testName
     //
-    // const [modalShow,setModalShow] = useState([false,false,false,false,false,false,false,false,false,false])
+     const [modalShow,setModalShow] = useState([false,false,false,false,false,false,false,false,false,false])
     // const size = 12
-    // const [dropdown, setDropdown] = useState([0,0,0,0,0,0,0,0,0,0]);
+
     //
     //
-    // const texts = textRepository.useRandomTextAndVisualization()
+     const texts = testRepository.get_test_properties(testName)
 
 
 
 
     return (
-        <div><h1>SHOW TEST</h1></div>
+     <div>
+    <h2 className="mb-3 text-left">Test: {testName}</h2><br/><br/><br/><br/>
+    {/*{texts && texts.data ? texts.data.map((text,index) => (*/}
+    {/*    <Row className="justify-content-center">*/}
+    {/*        <Col>*/}
+    {/*            <p>{text.name}</p>*/}
+    {/*        </Col>*/}
+    {/*        <Col>*/}
+    {/*            <Button onClick={(e)=>{*/}
+    {/*                let arr=[...modalShow]*/}
+    {/*                arr[text.id] = true*/}
+    {/*                setModalShow(arr)*/}
+    {/*            }}>Show Visualization</Button> <b>  Visualization: {text.visualization}</b><br/><br/>*/}
+    {/*            <RandomVisualizationDisplayModal visualization={text.visualization} show={modalShow[text.id]} onHide={() => {*/}
+    {/*                let arr=[...modalShow]*/}
+    {/*                arr[text.id] = false*/}
+    {/*                setModalShow(arr)*/}
+    {/*            }} text={text.id} threshold={text.threshold} propertyValue={text.propertyValue}></RandomVisualizationDisplayModal>*/}
+    {/*        </Col>*/}
+    {/*    </Row>*/}
+
+    {/*)) : null}*/}
+     </div>
         )
 
 }
