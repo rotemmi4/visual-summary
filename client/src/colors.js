ae192b16b22bf01c89caa830a14b305790361b0f
rotemmi4 <miarar@post.bgu.ac.il>
2021-05-30 15:55:12 +0300

    color

--- client/src/colors.js
+++ client/src/colors.js
@@ -1,36 +1,36 @@
 export const COLORS = {
     '1':{
         Yellow:['#FFE83B'],
-        Green:['#8be7a8']
+        Green:['#34d66d']
     },
     '3':{
-        Green:['#f7fcb9','#addd8e','#31a354'],
+        Green:['#f3ffcd','#a7ef85','#34d66d'],
         Yellow:['#ffffff','#ffff1a', '#ffe83b'],
         Orange:['#fff7bc','#fec44f','#d95f0e']
     },
     '4':{
         // Green:['#ffffff','#ffffd1','#8bc48c','#759f80'],
-        Green:['#ffffff','#f3ffcd','#a7ef85','#29c869'],
+        Green:['#ffffff','#f3ffcd','#a7ef85','#34d66d'],
         Yellow:['#ffffff','#fff176', '#ffff1a', '#fcdc00'],
         Orange:['#ffffff','#fff7bc','#fec44f','#d95f0e']
     },
     '5':{
-        Green:['#ffffcc','#c2e699','#78c679','#31a354','#006837'],
+        Green:['#ffffff','#f3ffcd','#a7ef85','#34d66d','#006837'],
         Yellow:['#ffffff','#fff9c4','#fff176', '#ffff1a', '#fcdc00'],
         Orange:['#ffffd4','#fed98e','#fe9929','#d95f0e','#993404']
     },
     '6':{
-        Green:['#ffffff','#ffffcc','#c2e699','#78c679','#31a354','#006837'],
+        Green:['#ffffff','#f3ffcd','#a7ef85','#34d66d','#31a354','#006837'],
         Yellow:['#ffffff','#fff9c4','#fff176', '#ffff1a','#fbff73', '#fcdc00'],
         Orange:['#ffffff','#ffffd4','#fed98e','#fe9929','#d95f0e','#993404']
     },
     '7':{
-        Green:['#ffffcc','#d9f0a3','#addd8e','#78c679','#41ab5d','#238443','#005a32'],
+        Green:['#ffffff','#f3ffcd','#a7ef85','#34d66d','#31a354','#006837','#005a32'],
         Yellow:['#ffffff','#fff9c4','#fff176', '#ffff1a','#fbff73', '#fff900','#fcdc00'],
         Orange:['#ffffd4','#fee391','#fec44f','#fe9929','#ec7014','#cc4c02','#8c2d04']
     },
     '8':{
-        Green:['#ffffff','#ffffcc','#d9f0a3','#addd8e','#78c679','#41ab5d','#238443','#005a32'],
+        Green:['#ffffff','#f3ffcd','#a7ef85','#34d66d','#31a354','#006837','#238443','#005a32'],
         Yellow:['#ffffff','#fff9c4','#fff176', '#ffff1a','#fbff73', '#fff900','#ffe700','#fcdc00'],
         Orange:['#ffffff','#ffffd4','#fee391','#fec44f','#fe9929','#ec7014','#cc4c02','#8c2d04']
     },
