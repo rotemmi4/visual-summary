27e0e2c981f88ab97223ac68362ca32c72d0878e
rotemmi4 <miarar@post.bgu.ac.il>
2021-06-06 01:05:02 +0300

    fixed font size

--- client/src/components/GradualFontVisualization.js
+++ client/src/components/GradualFontVisualization.js
@@ -6,8 +6,28 @@ export function GradualFontVisualization(props){
     const sentences = props.sentences
 
     return (
-        sentences.map(sentence => (
-            <span style={{whiteSpace: "pre-line" ,'font-size': + ((100 * sentence.weight) + 60) + '%'}}>{sentence.content} </span>
-        ))
+
+        sentences.map(sentence => {
+            if (sentence.weight <= 0.25)
+                return <span style={{
+                    whiteSpace: "pre-line",
+                    'font-size': '90%'
+                }}>{sentence.content} </span>
+            if (sentence.weight >0.25 &&  sentence.weight<= 0.5)
+                return <span style={{
+                    whiteSpace: "pre-line",
+                    'font-size': '120%'
+                }}>{sentence.content} </span>
+            if (sentence.weight> 0.5 && sentence.weight <= 0.75)
+                return <span style={{
+                    whiteSpace: "pre-line",
+                    'font-size': '150%'
+                }}>{sentence.content} </span>
+            if (sentence.weight> 0.75 && sentence.weight <= 1)
+                return <span style={{
+                    whiteSpace: "pre-line",
+                    'font-size': '180%'
+                }}>{sentence.content} </span>
+        })
     )
 }
